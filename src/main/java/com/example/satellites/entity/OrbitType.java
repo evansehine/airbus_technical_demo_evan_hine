919a0c797b5e8bbbfbba1f1a45eeaa167e50d602
evansehine <118451029+evansehine@users.noreply.github.com>
2026-03-04 01:06:59 +0000

    Update OrbitType.java with javadoc

--- src/main/java/com/example/satellites/entity/OrbitType.java
+++ src/main/java/com/example/satellites/entity/OrbitType.java
@@ -1,5 +1,12 @@
 package com.example.satellites.entity;
 
+/**
+ * Supported satellite orbit types.
+ * 
+ * LEO - Low Earth Orbit
+ * MEO - Medium Earth Orbit
+ * GEO - Geostationary Orbit
+ */
 public enum OrbitType {
     LEO,
     MEO,
