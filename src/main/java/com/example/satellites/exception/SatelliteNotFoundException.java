0b4763b219f315b0cdaba6d1d12810353e07e37d
evansehine <118451029+evansehine@users.noreply.github.com>
2026-03-04 01:08:31 +0000

    Update SatelliteNotFoundException.java

--- src/main/java/com/example/satellites/exception/SatelliteNotFoundException.java
+++ src/main/java/com/example/satellites/exception/SatelliteNotFoundException.java
@@ -1,6 +1,10 @@
 package com.example.satellites.exception;
 
+/**
+ * Exception thrown when a Satellite resource cannot be found.
+ */
 public class SatelliteNotFoundException extends RuntimeException {
+
     public SatelliteNotFoundException(Long id) {
         super("Satellite with id " + id + " not found");
     }
